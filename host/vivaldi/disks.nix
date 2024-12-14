8a1d5f7b69bcef9fd7f85aa2780367c6e0e2e017
wiessall <tristan.wiessalla@embl.it>
2024-12-14 18:47:01 +0100

    final?

---
+++ host/vivaldi/disks.nix
@@ -0,0 +1,93 @@
+{
+  lib,
+  disks ? [
+    "/dev/nvme0n1"
+  ],
+  ...
+}:
+let
+  cryptroot = "cryptroot";
+  defaultBtrfsOpts = [
+    "defaults"
+    "compress=zstd:1"
+    "ssd"
+    "noatime"
+    "nodiratime"
+  ];
+in
+{
+  boot.initrd.luks.devices.${cryptroot} = {
+    allowDiscards = true;
+    preLVM = true;
+  };
+
+  environment.etc = {
+    "crypttab".text = ''
+      data /dev/disk/by-partlabel/data /etc/data.keyfile
+    '';
+  };
+
+
+  disko.devices = {
+    disk = {
+      sda = {
+        device = builtins.elemAt disks 0;
+	type = "disk";
+	content = {
+	  type = "gpt";
+	  partitions = {
+	    ESP = {
+	      size = "512M";
+	      type = "EF00";
+	      content = {
+	        type = "filesystem";
+		format = "vfat";
+		mountpoint = "/boot";
+	      };
+	    };
+	    # Luks encrypted data partition/subvolumes
+	    luks = {
+	      size = "100%";
+	      content = {
+	        type = "luks";
+		name = "${cryptroot}";
+
+		settings = {
+		  allowDiscards = true;
+		};
+
+		content = {
+		  type = "btrfs";
+		  # Override existing partition
+		  extraArgs = [ "-f" ];
+		  subvolumes = {
+		    "@" = {
+		      mountpoint = "/";
+		      mountOptions = defaultBtrfsOpts;
+		    };
+		    "@nix" = {
+		      mountpoint = "/nix";
+		      mountOptions = defaultBtrfsOpts;
+		    };
+		    "@home" = {
+		      mountpoint = "/home";
+		      mountOptions = defaultBtrfsOpts;
+		    };
+		    "@var" = {
+		      mountpoint = "/var";
+		      mountOptions = defaultBtrfsOpts;
+		    };
+		    "@snapshots" = {
+		      mountpoint = "/.snapshots";
+		      mountOptions = defaultBtrfsOpts;
+		    };
+		  };
+		};
+       	      };
+       	    };
+          };
+        };
+      };
+    };
+  };
+}
