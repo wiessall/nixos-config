885226f5be7a7ad799f80f5408d7e1f3c08722d1
.wiess <tristan.wiess>
2024-12-16 10:35:51 +0000

    lsdkfj

--- host/common/users/tristan/default.nix
+++ host/common/users/tristan/default.nix
@@ -5,8 +5,7 @@ in
 {
   users.users.tristan = {
     isNormalUser = true;
-    initialHashedPassword = "$y$j9T$QFoOy2QF2BiTIU3KpAave0$keqTWJ9m23wWfWAjFr6r/7FtuFTECIN9ZqDSrIl8he7
-";
+    initialHashedPassword = "$y$j9T$QFoOy2QF2BiTIU3KpAave0$keqTWJ9m23wWfWAjFr6r/7FtuFTECIN9ZqDSrIl8he7";
 #    hashedPasswordFile = config.sops.secrets.tristan_passwd.path;
     shell = pkgs.zsh;
     extraGroups =
