f72eb0c1980fc60c669ac98493a35b6bc68da83e
t <>
2025-09-08 21:36:30 +0200

    builds

--- host/common/services/flatpaks.nix
+++ host/common/services/flatpaks.nix
@@ -15,11 +15,20 @@
       };
       packages = [
         "flathub:app/com.github.k4zmu2a.spacecadetpinball//stable"
-	"flathub:app/us.zoom.Zoom.desktop//stable"
-	"flathub:app/org.kde.kigo.desktop//stable"
-	"flathub:app/io.github.Qalculate//stable"
+        "flathub:app/us.zoom.Zoom.desktop//stable"
+        "flathub:app/org.kde.kigo.desktop//stable"
+        "flathub:app/io.github.Qalculate//stable"
+        "flathub:app/app.drey.Dialect//stable"
+        "flathub:app/com.rtosta.zapzap//stable"
+        "flathub:app/org.gnome.Snapshot//stable"
+        "flathub:app/com.nextcloud.desktopclient.nextcloud//stable"
+        "flathub:app/org.kde.kmahjongg//stable"
+        "flathub:app/org.kde.kpat//stable"
+        "flathub:app/org.gnome.Maps//stable"
+        "flathub:app/app.organicmaps.desktop//stable"
+        "flathub:app/org.chromium.Chromium//stable"
+        "flathub:app/org.mozilla.firefox//stable"
       ];
     };
   };
-
 }
